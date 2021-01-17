63fe8e6369af92f2ae66d779eeef7fc3b721c738
Bruno Rezende <33074471+brtec@users.noreply.github.com>
2021-01-16 21:08:18 -0300

    Implemented: Security Route
    Protected '/' Folder

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,16 @@
 const bodyParser = require('body-parser')
- 
+const security = require('./securityRoute')
 const pessoas = require('./pessoasRoute')
 const niveis = require('./niveisRoute')
 const turmas = require('./turmasRoute')
+
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     pessoas,
     niveis,
-    turmas
+    turmas,
+    security
   )
 }
